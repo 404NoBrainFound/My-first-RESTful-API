e26c50cb02ed349dba017cd759a8874a84f7cf8b
404NoBrainFound <125517019+404NoBrainFound@users.noreply.github.com>
2023-09-12 20:45:17 +0200

    Update user.js
    commented

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,45 +1,44 @@
+// Import necessary modules and middleware
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+// Import middleware for user authentication
 const checkAuth = require('../middleware/check-auth');
+
+// Import User model
 const User = require("../models/user");
 
-
-
+// User Signup Route
 router.post("/signup", (req, res, next) => {
-
+  // Check if email is already registered
   User.find({ email: req.body.email })
     .exec()
     .then(user => {
       if (user.length >= 1) {
+        // Email exists, return conflict status code
         return res.status(409).json({
           message: "Mail exists"
         });
-
-        
-
       } else {
-          // Salts password and adds callback
+        // Hash the password before saving it
         bcrypt.hash(req.body.password, 10, (err, hash) => {
-          // Password could not be stored or unsuccsefuly hashed
-            if (err) {
+          if (err) {
+            // Error during hashing
             return res.status(500).json({
               error: err
             });
-
-           // Creates new user with hashed password
           } else {
-        
+            // Create new User instance
             const user = new User({
               _id: new mongoose.Types.ObjectId(),
               email: req.body.email,
               password: hash,
             });
-            
-          
+
+            // Save user to database
             user
               .save()
               .then(result => {
@@ -48,7 +47,6 @@ router.post("/signup", (req, res, next) => {
                   message: "User created"
                 });
               })
-
               .catch(err => {
                 console.log(err);
                 res.status(500).json({
@@ -61,19 +59,20 @@ router.post("/signup", (req, res, next) => {
     });
 });
 
-
-
-// Login and verifaction
+// User Login Route
 router.post("/login", (req, res, next) => {
+  // Find user by email
   User.find({ email: req.body.email })
     .exec()
     .then(user => {
       if (user.length < 1) {
+        // User not found, return unauthorized status
         return res.status(401).json({
           message: "Auth failed"
         });
       }
-       
+
+      // Compare hashed passwords
       bcrypt.compare(req.body.password, user[0].password, (err, result) => {
         if (err) {
           return res.status(401).json({
@@ -81,18 +80,17 @@ router.post("/login", (req, res, next) => {
           });
         }
 
-        //JWT token
+        // Generate JWT if passwords match
         if (result) {
           const token = jwt.sign(
             {
-            email: user[0].email,
-            userId: user[0]._id
-          },
-          process.env.JWT_KEY,
-          {
-            expiresIn: "1h"
-          }
-
+              email: user[0].email,
+              userId: user[0]._id
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h"
+            }
           );
           return res.status(200).json({
             message: "Auth successful",
@@ -109,14 +107,13 @@ router.post("/login", (req, res, next) => {
       console.log(err);
       res.status(500).json({
         error: err
-      }); 
+      });
     });
 });
-        
-// Deletes user account
+
+// Delete User Route
 router.delete("/:userId", (req, res, next) => {
-  // Behaves like remove(), but deletes at most one document regardless of the single option.
-  // If this acts funny try deleteMany()
+  // Delete user by ID
   User.deleteOne({ _id: req.params.userId })
     .exec()
     .then(result => {
@@ -132,4 +129,5 @@ router.delete("/:userId", (req, res, next) => {
     });
 });
 
+// Export router for use in other modules
 module.exports = router;
