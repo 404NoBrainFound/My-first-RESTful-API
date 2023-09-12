a7bbb556936293575f1a3b788741eade3ddb6e38
404NoBrainFound <125517019+404NoBrainFound@users.noreply.github.com>
2023-09-12 20:47:56 +0200

    Update user.js
    commented

--- api/models/user.js
+++ api/models/user.js
@@ -1,14 +1,26 @@
+// Import mongoose library
 const mongoose = require('mongoose');
 
+// Create a new schema for User
 const userSchema = mongoose.Schema({
+    // Unique Identifier for the document
     _id: mongoose.Schema.Types.ObjectId,
+
+    // User email configuration
     email: { 
-        type: String, 
-        required: true, 
-        unique: true, 
+        type: String, // Expected data type
+        required: true, // This field cannot be empty
+        unique: true, // Email must be unique across all documents
+        // Regex pattern to validate email
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
-    password: { type: String, required: true }
+
+    // User password configuration
+    password: { 
+        type: String, // Expected data type
+        required: true // This field cannot be empty
+    }
 });
 
+// Export the model, usable in other parts of your application
 module.exports = mongoose.model('User', userSchema);
